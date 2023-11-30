d8827be980f6512216ee5b630369f8d278b06e56
Чингис <cianeed08@gmail.com>
2023-11-30 22:47:03 +0300

    fixed errors2

--- src/main/java/io/employees/HomeWork/EmployeeController.java
+++ src/main/java/io/employees/HomeWork/EmployeeController.java
@@ -18,31 +18,16 @@ public class EmployeeController {
 
     @GetMapping(path = "/add")
     public Employee add (@RequestParam("firstName")String firstName, @RequestParam("lastName") String lastName) {
-        try {
-            return employeeServiceImpl.addNewEmployee(firstName, lastName);
-        } catch (EmployeeAlreadyAddedException | EmployeeStorageIsFullException exception1) {
-            System.out.println(exception1.getMessage());
-        }
         return employeeServiceImpl.addNewEmployee(firstName, lastName);
     }
 
     @GetMapping(path = "/remove")
     public Employee remove (@RequestParam("firstName")String firstName, @RequestParam("lastName") String lastName) {
-        try {
-            return employeeServiceImpl.removeEmployee(firstName, lastName);
-        } catch (EmployeeNotFoundException exception2) {
-            System.out.println(exception2.getMessage());
-        }
         return employeeServiceImpl.removeEmployee(firstName, lastName);
     }
 
     @GetMapping(path = "/find")
     public Employee find (@RequestParam("firstName")String firstName, @RequestParam("lastName") String lastName) {
-        try {
-            return employeeServiceImpl.findEmployee(firstName, lastName);
-        } catch (EmployeeNotFoundException exception2) {
-            System.out.println(exception2.getMessage());
-        }
         return employeeServiceImpl.findEmployee(firstName, lastName);
     }
     @GetMapping
